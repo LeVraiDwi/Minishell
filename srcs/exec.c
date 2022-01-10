80eea6eb1e18f2f60d6d29019ef2a3002ab73395
Theo COSSE <tcosse@e2r6p11.clusters.42paris.fr>
2022-01-10 14:13:04 +0100

    exec et struct add

--- srcs/exec.c
+++ srcs/exec.c
@@ -1,43 +1,52 @@
 #include "minishell.h"
 
-void	ft_close_fd(int fd, int fd2)
+int	ft_error_pipe(t_parsing cmd)
 {
-	if (fd)
-		close(fd);
-	if (fd2)
-		close(fd2);
+	if (cmd->in > 2)
+		close(cmd->in);
+	if (cmd->out > 2)
+		close(cmd->out);
+	free_struct(cmd)
+	exit(EXIT_FAILURE);
 }
 
-int	ft_child(t_parsing cmd, t_term term, int nb_child)
+int	ft_child(t_parsing cmd, int nb_child, int	last_child)
 {
-	if (nb_child == 0)
-	{
-		if (cmd->in != 0)
-			if(dub2(cmd->in, STDIN_FD))
-				exit(EXIT_FAILURE);
-	}
+	int	status;
+
+	status = 0;
+	if(last_child != 0)
+		waitpid(last_child, &status, 0);
+	if (dub2(cmd->in, STDIN) < 0)
+		exit(ft_error_pipe(cmd));
+	if (dub2(cmd->out, STDOUT) < 0)
+		exit(ft_error_pipe(cmd));
+	execve(cmd->path, cmd->argv, cmd->env);
+	perror("ERROR:");
+	ft_free_struct(cmd);
+	exit(EXIT_FAILURE);
 }
 
-int	exec_tree(t_parsing cmd, t_term term)
+int	exec(t_parsing cmd)
 {
-	int				pipefd[2];
 	unsigned int	nb_pid;
 	int				child;
 	int				status;
+	int				last_child;
 
 	status = 0;
 	nb_child = 0;
-	if (pipe(pipefd) < 0)
-		return (0);
+	last_child = 0;
 	while (cmd)
 	{
 		child = fork();
 		if (child < 0)
 			return (0);
 		else if (child == 0)
-			ft_child(cmd, pipefd, nb_child);
+			ft_child(cmd, pipefd, nb_child, last_child);
 		nb_child++;
 		cmd = cmd->next;
+		last_child = child;
 	}
 	ft_close_fd(pipefd[0], pipefd[1]);
 	while (nb_child--)
@@ -45,6 +54,6 @@ int	exec_tree(t_parsing cmd, t_term term)
 		if (waitpid(-1, &status, 0) < 0)
 			exit(EXIT_FAILURE);
 	}
-	ft_free_pars(cmd);
+	ft_free_struct(cmd);
 	exit(EXIT_SUCCESS);
 }
---
+++ srcs/struct_pars.c
@@ -0,0 +1,62 @@
+#include <minishell.h>
+
+void	ft_init_struct(t_parsing *pars)
+{
+	pars->path = 0;
+	pars->argv = 0;
+	pars->env = 0;
+	pars->flag = 0;
+	pars->next = 0;
+	pars->in = 0;
+	pars->out = 0;
+	pars->str_in = 0;
+	pars->str_out = 0;
+}
+
+t_parsing	*ft_creat_pars(void)
+{
+	t_parsing *new;
+
+	new = (t_parsing *)malloc(sizeof(t_parsing) * 1);
+	if (!new)
+		return (0);
+	ft_init_struct(new);
+	return (new);
+}
+
+void	ft_free(void **p)
+{
+	if (*p)
+	{
+		free(*p);
+		*p = 0;
+	}
+}
+
+void	ft_free_argv(char ***argv)
+{
+	int	i;
+
+	i = 0;
+	if (argv)
+	{
+		while (argv[i])
+			ft_free(&argv[i++]);
+		ft_free(&argv);
+	}
+}
+
+int	ft_free_pars(t_parsing *pars)
+{
+	if (pars->next)
+		ft_rm_pars(pars->next);
+	if (pars->path)
+		ft_free(&pars->path);
+	ft_free_argv(pars->argv);
+	if (pars->str_in)
+		ft_free(&pars->str_in);
+	if (pars->str_out)
+		ft_free(&pars->str_out);
+	ft_free(&pars);
+	exit(EXIT_FAILURE);
+}
