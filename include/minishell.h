20e2d154ba819c73b3361fea74b827826ebb37ef
Theo COSSE <tcosse@e2r6p11.clusters.42paris.fr>
2022-01-10 14:24:06 +0100

    modif struct

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: asaboure <asaboure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 18:22:04 by asaboure          #+#    #+#             */
-/*   Updated: 2022/01/06 18:22:06 by asaboure         ###   ########.fr       */
+/*   Updated: 2022/01/10 13:59:20 by tcosse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,19 @@
 # include <fcntl.h>
 # include <sys/wait.h>
 # include <errno.h>
+# define STDIN 0
+# define STDOUT 1
 
 typedef struct	s_parsing
 {
-	char				**path; //le path
+	char				*path; //le path
 	char				**argv; //tableau des argument 1er element le nom de la cmd eg: echo
 	char				**env; //variable env du main
 	unsigned int		flag;
+	int					in;
+	int					out;
+	char				*str_in;
+	char				*str_out;
 	void				*next;
 }				t_parsing;
 
