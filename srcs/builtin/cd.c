4863fad28deeb9d23ecaf57d528640be2ca710de
Alexander SABOURET <asaboure@e2r9p10.clusters.42paris.fr>
2022-03-01 00:55:50 +0100

    fixed cd segfault

--- srcs/builtin/cd.c
+++ srcs/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: asaboure <asaboure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 13:20:33 by tcosse            #+#    #+#             */
-/*   Updated: 2022/02/28 20:28:29 by tcosse           ###   ########.fr       */
+/*   Updated: 2022/03/01 00:52:50 by asaboure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,41 @@
 int	ft_cd(t_term *term, t_parsing *parsing)
 {
 	int	l;
-	char	**cmd;
+	char	*path;
+	int	flag;
 
-	cmd = parsing->argv;
-	l = ft_cmd_length(cmd);
+	flag = 0;
+	path = parsing->argv[1];
+	l = ft_cmd_length(parsing->argv);
 	if (l > 2)
 	{
 		errno = E2BIG;
 		return (ft_perror());
 	}
-	if (!cmd[1])
+	if (!path)
 	{
-		cmd[1] = get_env_var(term->env, "HOME");
-		if (!cmd[1])
+		path = get_env_var(term->env, "HOME");
+		if (!path)
 		{
 			if (parsing->out > 1)
 				close(parsing->out);
 			parsing->out = 0;
 			return (-1);
 		}
+		flag = 1;
 	}
-	if (chdir(cmd[1]) < 0)
+	if (chdir(path) < 0)
 	{
 		perror("Error:");
 		if (parsing->out > 1)
 			close(parsing->out);
 		parsing->out = 0;
+		if (flag)
+			free(path);
 		return (-1);
 	}
+	if (flag)
+		free(path);
 	if (!ft_update_pwd(term))
 		return (-1);
 	if (parsing->out > 1)
