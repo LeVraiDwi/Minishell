1d8bf0920255cde0b79f05782125b76011c3fedb
Alexander SABOURET <asaboure@e2r9p10.clusters.42paris.fr>
2022-02-28 18:25:50 +0100

    norme

---
+++ srcs/utils/utils2.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asaboure <asaboure@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/28 18:21:01 by asaboure          #+#    #+#             */
+/*   Updated: 2022/02/28 18:21:37 by asaboure         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	ft_close(int fd1, int fd2)
+{
+	if (fd1 > 1)
+		close(fd1);
+	if (fd2 > 1)
+		close(fd2);
+	return (-1);
+}
+
+int	isnumber(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
