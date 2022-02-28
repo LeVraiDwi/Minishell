bfc099979a5204ff90cdbc628e1065d4276675d0
Alexander SABOURET <asaboure@e2r9p10.clusters.42paris.fr>
2022-02-28 18:40:34 +0100

    norme

--- include/exec.h
+++ include/exec.h
@@ -6,7 +6,7 @@
 /*   By: asaboure <asaboure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 18:28:10 by tcosse            #+#    #+#             */
-/*   Updated: 2022/02/28 16:05:37 by asaboure         ###   ########.fr       */
+/*   Updated: 2022/02/28 18:40:10 by asaboure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int		ft_checkright(char *path, int right);
 int		ft_openfile(char *path, int right, int mode, int flag);
 int		ft_stdopen(t_cmd *cmd, char *path);
 int		ft_open_std_cmd(t_cmd *cmd);
-int		creat_exec(t_term * term, t_cmd *cmd, t_parsing **exec, int *pipefd);
+int		creat_exec(t_term *term, t_cmd *cmd, t_parsing **exec, int *pipefd);
 int		ft_add_to_tab_cmd(t_parsing *exec, char *str);
 int		ft_add_redir(t_parsing *exec, t_cmd *cmd);
 int		ft_get_path(t_term *term, t_parsing *exec);
@@ -35,7 +35,7 @@ int		ft_init_pipe_out(t_parsing *exec, int *pipefd);
 int		ft_exec(t_term *term, t_parsing *exec);
 int		ft_close_exec(t_parsing *exec);
 void	signal_handler_child(int child);
-void	sigint_set_err();
+void	sigint_set_err(int sig);
 void	ft_set_pipe_in(t_parsing *exec, int *pipe);
 void	ft_select_std(t_parsing *execc, t_cmd *next);
 void	ft_dup_pipe(int *olf, int *new);
