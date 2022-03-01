2454e912de416d3e389813dc0c2ff3ca25aa6397
Theo COSSE <tcosse@e2r8p9.clusters.42paris.fr>
2022-03-01 02:18:12 +0100

    modif exec

--- srcs/expanser/expanser.c
+++ srcs/expanser/expanser.c
@@ -6,15 +6,22 @@
 /*   By: tcosse <tcosse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 16:28:34 by tcosse            #+#    #+#             */
-/*   Updated: 2022/02/27 22:42:18 by tcosse           ###   ########.fr       */
+/*   Updated: 2022/03/01 00:20:50 by tcosse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	expanser(t_term *term, t_cmd *cmd)
+int	expanser(t_term *term, t_cmd **tab)
 {
-	if (ft_replace_cmd(term, cmd) < 0)
-		return (-1);
+	int	i;
+
+	i = 0;
+	while (tab[i])
+	{
+		if (ft_replace_cmd(term, tab[i]) < 0)
+			return (-1);
+		i++;
+	}
 	return (0);
 }
