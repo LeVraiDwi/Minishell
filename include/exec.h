a9dff049cd63bd077341f21210eba9ae595c9c8f
Alexander SABOURET <asaboure@e3r8p3.clusters.42paris.fr>
2022-03-01 18:57:30 +0100

    removed double function declaration

--- include/exec.h
+++ include/exec.h
@@ -6,7 +6,7 @@
 /*   By: asaboure <asaboure@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 18:28:10 by tcosse            #+#    #+#             */
-/*   Updated: 2022/03/01 15:09:29 by tcosse           ###   ########.fr       */
+/*   Updated: 2022/03/01 18:56:50 by asaboure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,8 +42,6 @@ int		ft_is_exit(t_parsing *exec);
 int		ft_set_pipe(t_parsing *exec, int *pipefd, t_cmd *next);
 int		ft_select_built_exec(t_term *term,
 			t_parsing *exec, int in_pipe, t_cmd *next);
-void	signal_handler_child(int child);
-void	sigint_set_err(int sig);
 void	ft_set_pipe_in(t_parsing *exec, int *pipe);
 void	ft_select_std(t_parsing *execc, t_cmd *next);
 void	ft_dup_pipe(int *olf, int *new);
